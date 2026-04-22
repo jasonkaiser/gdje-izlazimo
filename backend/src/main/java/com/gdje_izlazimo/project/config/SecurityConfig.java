8c538ea796469c4b5960b159de13a51d80766c47
jasonkaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2026-04-22 10:54:53 +0200

    CORS updated

--- backend/src/main/java/com/gdje_izlazimo/project/config/SecurityConfig.java
+++ backend/src/main/java/com/gdje_izlazimo/project/config/SecurityConfig.java
@@ -64,8 +64,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
-        config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://gdje-izlazimo.onrender.com"
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
