91eeb4b42efee8879e48edcd05232c6a8d5851d6
jasonkaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2026-04-22 19:00:08 +0200

    Mobile UI Fixes

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, inject } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideAppInitializer } from '@angular/core';
 import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
-import { routes } from './app.routes';
+import { routes  } from './app.routes';
 import { AuthService } from './core/auth/auth.service';
 import { authInterceptor } from './core/http/interceptors/auth.interceptor';
 import { errorInterceptor } from './core/http/interceptors/error.interceptor';
@@ -11,7 +11,12 @@ import { loadingInterceptor } from './core/http/interceptors/loading.interceptor
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes,
+       withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled',
+      })
+    ),
     provideHttpClient( withFetch(),withInterceptors([authInterceptor, loadingInterceptor, errorInterceptor])),
     provideAppInitializer(() => {
       const authService = inject(AuthService);
